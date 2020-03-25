43f90340a6e344669f033461a5fbfa09b717d296
Aleksandrs Sergijenko <a.sergijenko6415@gmail.com>
2020-03-25 19:58:03 +0200

    Initial commit

---
+++ src/lesson15/Product.java
@@ -0,0 +1,101 @@
+package lesson15;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private BigDecimal price;
+    enum Category{FRUIT, VEGETABLES, DRINK, MEAT, FISH, SNACKS}
+    private BigDecimal discount;
+    private String description;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(long id, String name, BigDecimal price, BigDecimal discount, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        if (name != null ? !name.equals(product.name) : product.name != null) return false;
+        if (price != null ? !price.equals(product.price) : product.price != null) return false;
+        if (discount != null ? !discount.equals(product.discount) : product.discount != null) return false;
+        return description != null ? description.equals(product.description) : product.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (discount != null ? discount.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", discount=" + discount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/lesson15/ProductResponse.java
@@ -0,0 +1,21 @@
+package lesson15;
+
+public class ProductResponse {
+
+    private boolean success;
+    private String errorMessage;
+
+    public ProductResponse(boolean success,
+                           String errorMessage) {
+        this.success = success;
+        this.errorMessage = errorMessage;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
